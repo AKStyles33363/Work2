2cbbdbbdc144fdc0648393ba66c21e79b5ba4f4c
AKStyles33363 <67464257+AKStyles33363@users.noreply.github.com>
2020-06-26 15:44:09 +0300

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <meta charset="utf-8"/>
+    </head>    
+<body>
+        
+    <form action="/submit.php" method="POST">
+            <input name="email" placeholder="Email"><br>
+            <input name="name" placeholder="Name"><br>
+            <input name="phone" placeholder="Phone"><br>
+            <textarea name="message" placeholder="Message"></textarea><br>
+            <input type="submit" value="sent message"><br>
+        </form>
+        
+    </body>
+</html>
